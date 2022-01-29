00120a0fe1069100e8edd4a5acf0339dca2df20e
an112chuh <a.chuhnov@yandex.ru>
2022-01-30 02:58:06 +0300

    new push

--- main.go
+++ main.go
@@ -15,17 +15,17 @@ type QueueInfo struct {
 }
 
 type QueueWithMutex struct {
-	mx  sync.Mutex
+	mx  sync.RWMutex
 	Map map[string][]string
 }
 
 type LenWithMutex struct {
-	mx  sync.Mutex
+	mx  sync.RWMutex
 	Map map[string]int
 }
 
 type InfoGlobalWithMutex struct {
-	mx  sync.Mutex
+	mx  sync.RWMutex
 	Map map[string]QueueInfo
 }
 
@@ -44,13 +44,15 @@ func main() {
 		return
 	}
 	chName := make(chan int)
+
 	var Queue QueueWithMutex
 	var WaitingLens LenWithMutex
 	var InfoGlobal InfoGlobalWithMutex
+
 	Queue.Map = make(map[string][]string)
 	WaitingLens.Map = make(map[string]int)
 	InfoGlobal.Map = make(map[string]QueueInfo)
-	//	InfoLocal := make(map[string]QueueInfo)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		QueueName := r.URL.Path[1:]
 		switch r.Method {
@@ -67,41 +69,32 @@ func main() {
 		case http.MethodGet:
 			Queue.mx.Lock()
 			CurrentQueue, ok := Queue.Map[QueueName]
-			Queue.mx.Unlock()
 			if len(CurrentQueue) > 0 && ok {
 				fmt.Fprintf(w, CurrentQueue[0])
 				CurrentQueue[0] = ""
 				CurrentQueue = CurrentQueue[1:]
-				Queue.mx.Lock()
 				Queue.Map[QueueName] = CurrentQueue
 				Queue.mx.Unlock()
 				return
 			}
+			Queue.mx.Unlock()
 			WaitingLens.mx.Lock()
 			_, ok = WaitingLens.Map[QueueName]
-			WaitingLens.mx.Unlock()
 			if !ok {
-				WaitingLens.mx.Lock()
 				WaitingLens.Map[QueueName] = 1
-				WaitingLens.mx.Unlock()
 			} else {
-				WaitingLens.mx.Lock()
 				WaitingLens.Map[QueueName]++
-				WaitingLens.mx.Unlock()
 			}
-			WaitingLens.mx.Lock()
 			CurrentNumOfWaiting := WaitingLens.Map[QueueName]
 			WaitingLens.mx.Unlock()
 			InfoGlobal.mx.Lock()
 			Info, ok := InfoGlobal.Map[QueueName]
-			InfoGlobal.mx.Unlock()
 			if !ok || CurrentNumOfWaiting == 1 {
 				Info.Value = int(^uint(0) >> 1)
 				Info.IsChanged = -1
-				InfoGlobal.mx.Lock()
 				InfoGlobal.Map[QueueName] = Info
-				InfoGlobal.mx.Unlock()
 			}
+			InfoGlobal.mx.Unlock()
 			TimeOfTimeOut := r.FormValue("timeout")
 			if TimeOfTimeOut == "" {
 				http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
@@ -129,60 +122,53 @@ func main() {
 				InfoLocal.IsChanged = -1
 				p := 0
 				for {
+					fmt.Println("CNOW - " + strconv.Itoa(CurrentNumOfWaiting))
 					tmpValue := InfoLocal.Value
 					tmpIsChanged := InfoLocal.IsChanged
 					InfoGlobal.mx.Lock()
+					Queue.mx.Lock()
+					WaitingLens.mx.Lock()
 					InfoLocal = InfoGlobal.Map[QueueName]
-					InfoGlobal.mx.Unlock()
 					if tmpValue != InfoLocal.Value || tmpIsChanged != InfoLocal.IsChanged {
 						if InfoLocal.Value == CurrentNumOfWaiting {
 							if CurrentNumOfWaiting == 1 && WaitingLens.Map[QueueName] == 0 {
-								InfoGlobal.mx.Lock()
 								Info, ok := InfoGlobal.Map[QueueName]
-								InfoGlobal.mx.Unlock()
 								if ok {
 									Info.Value = int(^uint(0) >> 1)
-									InfoGlobal.mx.Lock()
 									InfoGlobal.Map[QueueName] = Info
-									InfoGlobal.mx.Unlock()
 								}
 							}
+							Queue.mx.Unlock()
+							WaitingLens.mx.Unlock()
+							InfoGlobal.mx.Unlock()
 							return
 						}
 						if CurrentNumOfWaiting > InfoLocal.Value {
 							CurrentNumOfWaiting--
 						}
 					}
-					Queue.mx.Lock()
+					Queue.mx.RLock()
 					CurrentQueue, ok := Queue.Map[QueueName]
-					Queue.mx.Unlock()
 					if ok {
 						if len(CurrentQueue) > 0 {
 							if CurrentNumOfWaiting == 1 {
+								fmt.Println("HERE")
 								fmt.Fprintf(w, CurrentQueue[0])
 								CurrentQueue[0] = ""
 								CurrentQueue = CurrentQueue[1:]
-								Queue.mx.Lock()
 								Queue.Map[QueueName] = CurrentQueue
-								Queue.mx.Unlock()
 								p = 1
-								WaitingLens.mx.Lock()
 								WaitingLens.Map[QueueName]--
 								tmp := WaitingLens.Map[QueueName]
-								WaitingLens.mx.Unlock()
 								if tmp >= 1 {
 									for i := 0; i < tmp; i++ {
 										chName <- 0
 									}
 								} else {
-									InfoGlobal.mx.Lock()
 									Info, ok := InfoGlobal.Map[QueueName]
-									InfoGlobal.mx.Unlock()
 									if ok {
 										Info.Value = int(^uint(0) >> 1)
-										InfoGlobal.mx.Lock()
 										InfoGlobal.Map[QueueName] = Info
-										InfoGlobal.mx.Unlock()
 									}
 
 								}
@@ -192,30 +178,34 @@ func main() {
 							}
 						}
 						if p == 1 {
+							Queue.mx.Unlock()
+							WaitingLens.mx.Unlock()
+							InfoGlobal.mx.Unlock()
 							cancel()
 							break
 						}
 					}
+					Queue.mx.Unlock()
+					WaitingLens.mx.Unlock()
+					InfoGlobal.mx.Unlock()
 				}
 			}()
 			select {
 			case <-ctx.Done():
 				http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 				InfoGlobal.mx.Lock()
+				WaitingLens.mx.Lock()
 				Info, ok := InfoGlobal.Map[QueueName]
-				InfoGlobal.mx.Unlock()
 				if ok {
 					if InfoGlobal.Map[QueueName].Value == CurrentNumOfWaiting {
 						Info.IsChanged *= (-1)
 					} else {
 						Info.Value = CurrentNumOfWaiting
 					}
-					InfoGlobal.mx.Lock()
 					InfoGlobal.Map[QueueName] = Info
-					InfoGlobal.mx.Unlock()
 				}
-				WaitingLens.mx.Lock()
 				WaitingLens.Map[QueueName]--
+				InfoGlobal.mx.Unlock()
 				WaitingLens.mx.Unlock()
 				cancel1()
 				return
